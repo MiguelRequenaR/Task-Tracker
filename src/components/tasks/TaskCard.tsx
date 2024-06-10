620922304e5318de91bca69b967e1be44bf20b76
Miguel Angel Requena Ramos <m.angelrequena20@gmail.com>
2024-06-10 13:32:05 -0500

    Drag & drop interfaces agregado

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteTask } from "@/api/TaskApi"
 import { toast } from "react-toastify"
+import { useDraggable } from "@dnd-kit/core"
 
 type TaskCardProps = {
     task: Task
@@ -15,6 +16,9 @@ type TaskCardProps = {
 
 export default function TaskCard({task, canEdit}: TaskCardProps) {
 
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+        id: task._id,
+    })
     const navigate = useNavigate();
     const params = useParams();
     const projectId = params.projectId!;
@@ -31,9 +35,22 @@ export default function TaskCard({task, canEdit}: TaskCardProps) {
         }
     })
 
+    const style = transform ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
+    }: undefined;
+
     return (
-        <li className="flex justify-between p-5 bg-white border border-slate-300 gap-3">
-            <div className="min-w-0 flex flex-col gap-y-4">
+        <li 
+            className="flex justify-between p-5 bg-white border border-slate-300 gap-3 rounded-xl"
+            {...listeners}
+            {...attributes}
+            ref={setNodeRef}
+            style={style}
+        >
+            <div 
+                
+                className="min-w-0 flex flex-col gap-y-4"
+            >
                 <button
                     type="button"
                     className="text-xl font-bold text-slate-600 text-left"
--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,6 +1,19 @@
-import { Task } from "@/types"
+import { Task, TaskStatus } from "@/types"
 import TaskCard from "./TaskCard";
 import { statusTranslations } from "@/locales/translations";
+import DropTask from "./DropTask";
+import { 
+    DndContext, 
+    DragEndEvent, 
+    MouseSensor, 
+    TouchSensor, 
+    useSensor, 
+    useSensors 
+} from "@dnd-kit/core";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateStatus } from "@/api/TaskApi";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 
 type TaskListProps = {
@@ -33,6 +46,21 @@ const statusStyles : { [key: string] : string } = {
 
 export default function TaskList({tasks, canEdit}: TaskListProps) {
 
+    const params = useParams();
+    const projectId = params.projectId!;
+
+    const queryClient = useQueryClient();
+    const { mutate } = useMutation({
+        mutationFn: updateStatus,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+            queryClient.invalidateQueries();
+        }
+    })
+
     //Agrupar las tareas por el estado con reduce
     const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -40,28 +68,63 @@ export default function TaskList({tasks, canEdit}: TaskListProps) {
         return { ...acc, [task.status]: currentGroup };
     }, initialStatusGroups);
 
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: {
+            distance: 10,
+        }
+    });
+
+    const touchSensor = useSensor(TouchSensor, {
+        activationConstraint: {
+            delay: 250,
+            tolerance: 5,
+        }
+    });
+
+    const sensors = useSensors(mouseSensor, touchSensor);
+
+    const handleDragEnd = (e: DragEndEvent) => {
+        const { over, active } = e;
+        if(over && over.id){
+            const taskId = active.id.toString();
+            const status = over.id as TaskStatus;
+
+            mutate({projectId, taskId, status});
+        }
+    }
+
 
     return (
         <>
             <h2 className="text-5xl font-black text-primary my-10">Tareas Disponibles</h2>
 
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
-                {Object.entries(groupedTasks).map(([status, tasks]) => (
-                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                        <h3 
-                            className={`capitalize text-xl font-semibold text-gray-700 text-center border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
-                        >
-                            {statusTranslations[status]}
-                        </h3>
-                        <ul className='mt-5 space-y-5'>
-                            {tasks.length === 0 ? (
-                                <li className="text-gray-500 text-center font-semibold pt-3">No hay tareas</li>
-                            ) : (
-                                tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
-                            )}
-                        </ul>
-                    </div>
-                ))}
+                <DndContext
+                    sensors={sensors}
+                    onDragEnd={(handleDragEnd)}
+                >
+                    {Object.entries(groupedTasks).map(([status, tasks]) => (
+                        <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                            <h3 
+                                className={`capitalize text-xl font-semibold text-gray-700 text-center border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                            >
+                                {statusTranslations[status]}
+                            </h3>
+
+                            <DropTask 
+                                status={status}
+                            />
+
+                            <ul className='mt-5 space-y-5'>
+                                {tasks.length === 0 ? (
+                                    <li className="text-gray-500 text-center font-semibold pt-3">No hay tareas</li>
+                                ) : (
+                                    tasks.map(task => <TaskCard key={task._id} task={task} canEdit={canEdit} />)
+                                )}
+                            </ul>
+                        </div>
+                    ))}
+                </DndContext>
             </div>
         </>
     )
