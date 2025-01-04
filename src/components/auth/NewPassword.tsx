276d01cd34e4a1d78034a126d6ca5e4f9c15d1de
MiguelRequenaR <e_2019100844L@uncp.edu.pe>
2025-01-03 22:39:04 -0500

    Refactor NewPassword component: remove unused - Fix Production Error

--- src/components/auth/NewPassword.tsx
+++ src/components/auth/NewPassword.tsx
@@ -32,19 +32,6 @@ export default function NewPassword({token, setToken, setIsValid}: NewPasswordPr
         mutate({token});
     }
 
-    const validateTokenRequest = async (token: string) => {
-        try {
-            await validateToken({ token });
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            } else {
-                toast.error('Error al validar el token');
-            }
-            // Redirigir al usuario o manejar el error según sea necesario
-        }
-    }
-
     return (
         <>
             <div className='bg-tertiary p-10 rounded-xl'>  
